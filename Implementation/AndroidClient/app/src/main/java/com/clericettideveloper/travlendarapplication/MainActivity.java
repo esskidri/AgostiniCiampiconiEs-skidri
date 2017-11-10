9eff0de8bb430d2a584a31b1ad79e14587b1702b
agostini95 <andrea1.agostini@mail.polimi.it>
2017-11-10 19:09:02 +0100

    JSON Retrieving from MyLambdaExpression

--- Implementation/AndroidClient/app/src/main/java/com/clericettideveloper/travlendarapplication/MainActivity.java
+++ Implementation/AndroidClient/app/src/main/java/com/clericettideveloper/travlendarapplication/MainActivity.java
@@ -3,6 +3,11 @@ package com.clericettideveloper.travlendarapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 import java.util.Map;
 
@@ -10,6 +15,7 @@ import io.skygear.skygear.AuthResponseHandler;
 import io.skygear.skygear.Configuration;
 import io.skygear.skygear.Container;
 import io.skygear.skygear.Error;
+import io.skygear.skygear.LambdaResponseHandler;
 import io.skygear.skygear.Record;
 import io.skygear.skygear.RecordSaveResponseHandler;
 
@@ -26,10 +32,18 @@ public class MainActivity extends AppCompatActivity {
                 .endPoint("https://travlendar.skygeario.com/")
                 .apiKey("90bbd0894c6c408abf4c56f82df18d6f")
                 .build();
-
         Container.defaultContainer(this).configure(config);
 
-        /*Sky-gear insert a test row in the DB*/
+        /*Button configuration*/
+        final Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                serverCall();
+            }
+        });
+
+        /****************************************************************************************/
+        /*Sky-gear insert a test row in the DB*//*
         final Container skygear = Container.defaultContainer(this);
         skygear.getAuth().signupAnonymously(new AuthResponseHandler() {
         @Override
@@ -64,7 +78,29 @@ public class MainActivity extends AppCompatActivity {
             public void onAuthFail(Error error) {
                 Log.w("MyApplication", "Failed to signup: " + error.getMessage(), error);
             }
-    });
+    });*/
+
+    }
+
+
+    private void serverCall(){
+        /*SkyGear retrieving data from my server lambdaExpression*/
+
+        String lambdaName = "hello_from_server";
+        final Container skygear = Container.defaultContainer(this);
+        skygear.callLambdaFunction(lambdaName, new LambdaResponseHandler() {
+            @Override
+            public void onLambdaSuccess(JSONObject result) {
+                System.out.println("hello from server ok");
+                System.out.println(result.toString());
+            }
+
+            @Override
+            public void onLambdaFail(Error error) {
+                System.out.println("hello from server ko");
+
+            }
+        });
     }
 
 }
