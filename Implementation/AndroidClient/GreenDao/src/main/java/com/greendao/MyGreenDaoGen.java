33f8daa80800bc4e2bd9cbfd904027d9eec209bc
LorenzoCiampiconi <lorenzo.ciampiconi@mail.polimi.it>
2017-11-17 13:17:26 +0100

    Gradle Settings

--- Implementation/AndroidClient/GreenDao/src/main/java/com/greendao/MyGreenDaoGen.java
+++ Implementation/AndroidClient/GreenDao/src/main/java/com/greendao/MyGreenDaoGen.java
@@ -9,6 +9,28 @@ public class MyGreenDaoGen {
         Schema schema = new Schema(1, "com.clericettideveloper.travlendarapplication.db");
         schema.enableKeepSectionsByDefault();
 
+        addTables(schema);
 
+        try {
+            new DaoGenerator().generateAll(schema,"./app/src/main/java");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    private static void addTables(final Schema schema) {
+        addUserEntities(schema);
+    }
+
+
+    private static Entity addUserEntities(final Schema schema) {
+        Entity user = schema.addEntity("User");
+
+        return user;
+    }
+
+
+    /*private static Entity addCalendarEntities(final Schema schema) {
+
+    }*/
 }
