15f6bc88df54bef6122d13df21e186d26e4d43e4
agostini95 <andrea1.agostini@mail.polimi.it>
2017-12-02 00:54:16 +0100

    jpa correctly configured

---
+++ Implementation/travlendarServer/src/main/java/com/travlendar/travlendarServer/controller/HelloGreeting.java
@@ -0,0 +1,12 @@
+package com.travlendar.travlendarServer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloGreeting {
+    @RequestMapping("/greet")
+    public String helloGreeting() {
+        return "Hello REST";
+    }
+}
