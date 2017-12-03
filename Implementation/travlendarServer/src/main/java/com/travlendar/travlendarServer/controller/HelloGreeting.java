91aee523ae022a51154ff68682f5b725af661fe3
esskidri <esskidri@gmail.com>
2017-12-03 20:53:13 +0100

    ok

--- Implementation/travlendarServer/src/main/java/com/travlendar/travlendarServer/controller/HelloGreeting.java
+++ Implementation/travlendarServer/src/main/java/com/travlendar/travlendarServer/controller/HelloGreeting.java
@@ -1,19 +1,21 @@
 package com.travlendar.travlendarServer.controller;
 
-import com.sun.jmx.snmp.Timestamp;
+
 import com.travlendar.travlendarServer.model.Event;
 import com.travlendar.travlendarServer.model.TransportSolution;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.sql.Timestamp;
+
 
 @RestController
 public class HelloGreeting {
     @RequestMapping("/greet")
     public String helloGreeting() {
-        Timestamp t= new Timestamp(11111111);
-        Event e = new Event(1, t, t, 1.0,1.0, "okok", new TransportSolution(1,1), new TransportSolution(2,1),true);
+        Timestamp t = new Timestamp((System.currentTimeMillis()));
+        Event e = new Event(1, t,t, (float) 1.000,(float)1.000, "okok", new TransportSolution(1,1), new TransportSolution(2,1),1,"rax");
+
         return "Hello REST";
     }
 
