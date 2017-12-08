dcbe2b6c69a82bcb98ced148f932275a8abf4bcd
LorenzoCiampiconi <lorenzo.ciampiconi@mail.polimi.it>
2017-12-08 17:14:38 +0100

    First connection between Logic and Data modules try

---
+++ Implementation/travlendarServer/src/main/java/com/travlendar/travlendarServer/logic/MainLogic.java
@@ -0,0 +1,14 @@
+package com.travlendar.travlendarServer.logic;
+
+import com.travlendar.travlendarServer.model.domain.Event;
+import com.travlendar.travlendarServer.model.domain.TransportSolution;
+import com.travlendar.travlendarServer.model.domain.User;
+
+import java.util.List;
+
+public class MainLogic {
+
+    public List<TransportSolution> calculateTransportSolution(List<Event> events, User user){
+        return null;
+    }
+}
