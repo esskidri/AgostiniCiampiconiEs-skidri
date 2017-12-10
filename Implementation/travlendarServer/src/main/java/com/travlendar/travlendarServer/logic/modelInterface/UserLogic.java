a1f47a1b186ea1c2d6f0da91af2154468bfb820b
LorenzoCiampiconi <lorenzo.ciampiconi@mail.polimi.it>
2017-12-10 15:46:38 +0100

    First Version finished of solution calculator

--- Implementation/travlendarServer/src/main/java/com/travlendar/travlendarServer/logic/modelInterface/UserLogic.java
+++ Implementation/travlendarServer/src/main/java/com/travlendar/travlendarServer/logic/modelInterface/UserLogic.java
@@ -1,7 +1,5 @@
 package com.travlendar.travlendarServer.logic.modelInterface;
 
-import com.travlendar.travlendarServer.model.domain.UserPreferences;
-
 import java.util.List;
 
 public interface UserLogic {
--- Implementation/travlendarServer/src/main/java/com/travlendar/travlendarServer/model/domain/UserPreferences.java
+++
@@ -1,4 +0,0 @@
-package com.travlendar.travlendarServer.model.domain;
-
-public class UserPreferences {
-}
