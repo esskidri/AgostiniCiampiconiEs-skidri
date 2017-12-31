81d0418a22b2f1b3e539b36af04855ad275f8f6d
esskidri <esskidri@gmail.com>
2017-12-31 13:57:57 +0100

    set policy added

--- Implementation/travlendarServer/src/main/java/com/travlendar/travlendarServer/controller/dataManager/RequestHandler.java
+++ Implementation/travlendarServer/src/main/java/com/travlendar/travlendarServer/controller/dataManager/RequestHandler.java
@@ -364,6 +364,35 @@ public class RequestHandler {
         return r.getMessage();
     }
 
+    @RequestMapping("/set-policy")
+    @ResponseBody
+    public String addFreeTime(@RequestParam("user_id") Long userId,
+                              @RequestParam("policy") String policy) {
+        Response r=new Response("ok");
+        try{
+            //fetch the user
+            User u = userDao.findOne(userId);
+            policy = policy.toUpperCase();
+            switch (policy){
+                case "GREEN": u.setPolicy(Policy.GREEN);
+                              break;
+                case "FAST":  u.setPolicy(Policy.FAST);
+                              break;
+                case "CHEAP": u.setPolicy(Policy.CHEAP);
+                              break;
+                default:      u.setPolicy(Policy.GREEN);
+                              break;
+            }
+            userDao.save(u);
+            r.setMessage("policy setted");
+        }catch(Exception e2){
+            r.setMessage("fail"+e2.getMessage());
+        }
+        return r.getMessage();
+    }
+
+
+
     @RequestMapping("/add-free-time")
     @ResponseBody
     public String addFreeTime(@RequestParam("user_id") Long userId,
